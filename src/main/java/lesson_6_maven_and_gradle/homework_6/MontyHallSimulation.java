efc7a61d1e9e977e9b0622ccb97de8d55a69501f
Simeon Slashchinin <sem63sam@yandex.ru>
2024-06-21 23:27:38 +0400

    add map to solution

--- src/main/java/lesson_6_maven_and_gradle/homework_6/MontyHallSimulation.java
+++ src/main/java/lesson_6_maven_and_gradle/homework_6/MontyHallSimulation.java
@@ -4,33 +4,37 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class MontyHallSimulation {
 
     private static final int NUM_SIMULATIONS = 1000;
     private static final Random RANDOM = new Random();
 
-public static void main(String[] args) {
-    int stayWins = 0;
-    int switchWins = 0;
+    public static void main(String[] args) {
+        Map<Integer, Boolean> results = new HashMap<>();
 
-    for (int i = 0; i < NUM_SIMULATIONS; i++) {
-        if (simulateGame(false)) {
-            stayWins++;
+        for (int i = 0; i < NUM_SIMULATIONS; i++) {
+            boolean stayWin = simulateGame(false);
+            results.put(i, stayWin);
+
+            boolean switchWin = simulateGame(true);
+            results.put(i + NUM_SIMULATIONS, switchWin);
         }
-        if (simulateGame(true)) {
-            switchWins++;
-        }
+
+        long stayWins = countWins(results, 0, NUM_SIMULATIONS);
+        long switchWins = countWins(results, NUM_SIMULATIONS, 2 * NUM_SIMULATIONS);
+
+        double stayWinPercentage = Math.round((stayWins / (double) NUM_SIMULATIONS) * 100);
+        double switchWinPercentage = Math.round((switchWins / (double) NUM_SIMULATIONS) * 100);
+
+        System.out.println("Выигрывает, оставаясь при первоначальном выборе: " + stayWins + " (" + stayWinPercentage + "%)");
+        System.out.println("Выигрывает при смене выбора: " + switchWins + " (" + switchWinPercentage + "%)");
+
     }
-
-    double stayWinPercentage = Math.round((stayWins / (double) NUM_SIMULATIONS) * 100);
-    double switchWinPercentage = Math.round((switchWins / (double) NUM_SIMULATIONS) * 100);
-
-    System.out.println("Выигрывает, оставаясь при первоначальном выборе: " + stayWins + " (" + stayWinPercentage + "%)");
-    System.out.println("Выигрывает при смене выбора: " + switchWins + " (" + switchWinPercentage + "%)");
-
-}
 
     private static boolean simulateGame(boolean switchChoice) {
         Game game = Game.builder()
@@ -42,6 +46,12 @@ public static void main(String[] args) {
             game.switchDoor();
         }
         return game.isWin();
+    }
+
+    private static long countWins(Map<Integer, Boolean> results, int start, int end) {
+        return IntStream.range(start, end)
+                .filter(results::get)
+                .count();
     }
 
     @Data
